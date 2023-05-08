feaffb2ea69aff70def31c6b3f21c7420a028e0e
DaniDhax <111738016+DaniDhax@users.noreply.github.com>
2023-05-08 11:51:40 +0000

    Part2.b.Ejercicio 2.6

--- src/App.js
+++ src/App.js
@@ -1,57 +1,24 @@
 import React, { useState } from 'react'
-import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
-
-  //Función para añadir una nueva nota (objeto) al array de notas (array de objetos 'nota'):
-  const addNote = (event) => {
-    event.preventDefault()
-    if (newNote === '') { return }
-
-    //Crear un nuevo objeo con las propiedades del objeto 'nota':
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5, //Esta expresión devuelve True o False (50% de las veces cada uno)
-      id: notes.length + 1,
-    }
-
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-  }
-
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value)
-  }
-
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important === true)
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
 
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
-        </button>
-      </div>
-
-      <ul>
-        {notesToShow.map(note => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type='submit'>save</button>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
       </form>
+      <h2>Numbers</h2>
+      ...
     </div>
   )
 }
