96f226ec8a48c08d3e1f838712e858ec1d085e04
DaniDhax <111738016+DaniDhax@users.noreply.github.com>
2023-05-11 12:09:30 +0000

    Part2.b.Ejercicio 2.9

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+// import { computeHeadingLevel } from '@testing-library/react'
 import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567'  
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
+
   const [newName, setNewName] = useState('')
+  const [filter, setFilter] = useState([])
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    const personsFiltered = persons.filter(person => person.name.toLowerCase().startsWith((event.target.value).toLowerCase()))
+    setFilter (personsFiltered)
+    console.log (personsFiltered)
+  }
 
   const addNewName = (event) => {
     event.preventDefault()
     const varNombre = document.getElementById('txtNombre').value
     const varTlfno = document.getElementById('txtTlfno').value
+    
     for (let i = 0; i < persons.length; i++) {
       if (persons[i].name === varNombre) {
         console.log (`${varNombre} is already added to phonebook`)
@@ -26,12 +37,17 @@ const App = () => {
       number: varTlfno
     }
     setPersons(persons.concat(person))
+    document.getElementById('txtNombre').value = ''
+    document.getElementById('txtTlfno').value = ''
   }
-
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with<input id='txtFiltro' onChange={handleFilterChange}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addNewName}>
         <div>name: <input id='txtNombre' /></div>
         <div>number: <input id='txtTlfno' /></div>
@@ -39,8 +55,13 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
 
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
-
+        {
+        document.getElementById('txtFiltro') && document.getElementById('txtFiltro').value !== '' ? 
+        filter.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+        : 
+        persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+        }
+    
     </div>
   )
 }
