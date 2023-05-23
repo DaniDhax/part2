606f9ce9b2c5bbe3c0d7390c57330a67089061f0
DaniDhax <111738016+DaniDhax@users.noreply.github.com>
2023-05-23 12:03:40 +0000

    Part2.c.Ejercicio 2.12 (4)

--- src/App.js
+++ src/App.js
@@ -3,24 +3,51 @@ import axios from "axios"
 
 const App = () => {
     const [paises, setPaises] = useState([])
+    const [paisesFiltrados, setPaisesFiltrados] = useState([])
+    const [loading, setLoading] = useState(true) // Variable de estado para el estado de carga
 
     useEffect(() => {
-        axios
-            .get('https://restcountries.com/v3.1/all')
-            .then(response => {
-                setPaises(response.data)
-            })
-    }, [])
+        axios.get("https://studies.cs.helsinki.fi/restcountries/api/all").then((response) => {
+            setPaises(response.data);
+            setLoading(false); // Una vez que los datos se han descargado, establecer loading en false
+        });
+    }, []);
+
+    const handleChange = (event) => {
+        let value = event.target.value;
+        const filtrados = paises.filter(pais =>
+            pais.name.common.toLowerCase().includes(value.toLowerCase())
+        );
+        setPaisesFiltrados(filtrados);
+    };
 
     return (
         <div>
-            <ol>
-                {paises.map((pais) => {
-                    return (
-                        <li key={pais.name.common}>{pais.name.common}</li>)
-                })}
-            </ol>
-        </div>
+        <p>
+          Buscar país: <input type="text" id="txtBuscarPais" onChange={handleChange}></input>
+        </p>
+        {loading ? (
+          <p>Obteniendo datos del servidor...</p>
+        ) : (
+          <>
+            <p>{paisesFiltrados.length}</p>
+            {paisesFiltrados.length > 0 ? (
+              paisesFiltrados.length < 11 ? (
+                <ol>
+                  {paisesFiltrados.map((pais) => {
+                    return <li key={pais.name.common}>{pais.name.common}</li>;
+                  })}
+                </ol>
+              ) : (
+                <p>Demasiadas respuestas, especifique otro filtro</p>
+              )
+            ) : (
+              <p>No se encontraron resultados</p>
+            )}
+          </>
+        )}
+      </div>
+
     )
 }
 
